3479669000d71403fc6246c67f6775710c2d583f
jenniferthorpe <jennifer.thorpe@hotmail.com>
2019-08-26 12:36:31 +0200

    simple gatsby site

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,7 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `purple`, fontSize: `72px` }}>
-    <Link to="/contact/">Contact</Link>
+    <Link to="/">Home</Link>
     <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
